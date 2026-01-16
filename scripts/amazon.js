2d4e22b4ff2c9a1f5b6295dc6fce11dd83764e29
towfique-pranto <towfique.pranto@gmail.com>
2026-01-17 02:13:15 +0600

    added $ sign infront of price

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -22,7 +22,7 @@ products.forEach((product)=>{
             <div class="product-rating-count link-primary">${product.rating.count}</div>
           </div>
 
-          <div class="product-price">${(product.priceCents / 100).toFixed(2)}</div>
+          <div class="product-price">$${(product.priceCents / 100).toFixed(2)}</div>
 
           <div class="product-quantity-container">
             <select>
