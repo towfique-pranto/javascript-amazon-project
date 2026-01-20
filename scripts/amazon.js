983c75ec6a138be3f511b2eb51ebac333239f26e
towfique-pranto <towfique.pranto@gmail.com>
2026-01-21 02:06:29 +0600

    Moved cart quantity calculation in one function and exported to other files

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,4 @@
-import {cart, addToCart} from '../data/cart.js';
+import {cart, addToCart, calculateCartQuantity} from '../data/cart.js';
 import { products } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 
@@ -60,12 +60,11 @@ document.querySelector('.js-products-grid').innerHTML = productsHTML;
 updateCartQuantity();
 
 function updateCartQuantity (){
-  let cartQuantity = 0;
-    cart.forEach((cartItem)=>{
-      cartQuantity += cartItem.quantity;
-    });
-    document.querySelector('.js-cart-quantity').innerText = cartQuantity;
+  let cartQuantity = calculateCartQuantity();
+  document.querySelector('.js-cart-quantity').innerText = cartQuantity;
   };
+
+  
 
 const allTimeoutId = {};
 document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart, removeFromCart} from '../data/cart.js';
+import {cart, removeFromCart, calculateCartQuantity} from '../data/cart.js';
 import {products} from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 
@@ -102,9 +102,6 @@ document.querySelectorAll('.js-delete-link').forEach((link)=>{
 });
 
 function updateCheckoutQuantity (){
-  let cartQuantity = 0;
-    cart.forEach((cartItem)=>{
-      cartQuantity += cartItem.quantity;
-    });
+  let cartQuantity = calculateCartQuantity();
     document.querySelector('.js-return-to-home').innerText = cartQuantity + ' items';
   };
