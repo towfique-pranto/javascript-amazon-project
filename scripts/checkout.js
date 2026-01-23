c809a7247022c5ffd6c460cb4600f3733d417391
towfique-pranto <towfique.pranto@gmail.com>
2026-01-24 01:45:14 +0600

    make delivery options interactive

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart, removeFromCart, calculateCartQuantity, updateQuantity} from '../data/cart.js';
+import {cart, removeFromCart, calculateCartQuantity, updateQuantity, updateDeliveryOption} from '../data/cart.js';
 import {products} from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
@@ -95,7 +95,9 @@ function deliveryOptionsHTML(matchingProduct, cartItem){
   const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
 
     html +=`
-      <div class="delivery-option">
+      <div class="delivery-option js-delivery-option" 
+      data-product-id="${matchingProduct.id}"
+      data-delivery-option-id="${deliveryOption.id}">
         <input
           type="radio"
           ${isChecked ? 'checked' : ''}
@@ -171,3 +173,11 @@ function saveNewQuantity(productId, container){
     container.querySelector('.js-quantity-input').classList.add('is-invalid');
   }
 };
+
+document.querySelectorAll('.js-delivery-option').forEach((element)=>{
+  element.addEventListener('click', ()=>{
+    const {productId, deliveryOptionId} = element.dataset;
+    updateDeliveryOption(productId, deliveryOptionId);
+  });
+
+});
