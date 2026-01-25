ed26fae2f32c8d469f9331ed6438ce1dc5bf12ae
towfique-pranto <towfique.pranto@gmail.com>
2026-01-26 03:46:21 +0600

    changed html render method and added Dynamic Injection value in payment item quantity

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -140,7 +140,7 @@ export function renderOrderSummary(){
 
       const container = document.querySelector(`.js-cart-item-container-${productId}`);
       container.remove();
-      updateCheckoutQuantity();
+      renderOrderSummary();
       renderPaymentSummary();
     });
   });
--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -6,10 +6,12 @@ import {formatCurrency} from "../utils/money.js"
 export function renderPaymentSummary(){
   let productPriceCents = 0;
   let shippingPriceCents = 0;
+  let itemQuantity = 0;
 
   cart.forEach((cartItem) => {
     const product = getProduct(cartItem.productId);
     productPriceCents += product.priceCents * cartItem.quantity;
+    itemQuantity += cartItem.quantity;
 
     const deliveryOption = getDeliveryOption(cartItem.deliveryOptionId);
     shippingPriceCents += deliveryOption.priceCents;
@@ -22,7 +24,7 @@ export function renderPaymentSummary(){
   const paymentSummaryHTML = `
     <div class="payment-summary-title">Order Summary</div>
       <div class="payment-summary-row">
-        <div>Items (3):</div>
+        <div>Items (${itemQuantity}):</div>
         <div class="payment-summary-money">$${formatCurrency(productPriceCents)}</div>
       </div>
 
