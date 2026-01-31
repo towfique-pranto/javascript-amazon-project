e874da5cf1827229e2f02c0bd0b935828ae70fe1
towfique-pranto <towfique.pranto@gmail.com>
2026-01-31 23:01:47 +0600

    load products from the backend

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,9 +1,14 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCheckoutHeader } from './checkout/checkoutHeader.js';
-//import '../data/car.js';
-import '../data/backend-practice.js';
+import { loadProducts } from "../data/products.js";
 
-renderCheckoutHeader();
-renderOrderSummary();
-renderPaymentSummary();
+//import '../data/car.js';
+//import '../data/backend-practice.js';
+
+loadProducts(() => {
+  renderCheckoutHeader();
+  renderOrderSummary();
+  renderPaymentSummary();
+});
+
